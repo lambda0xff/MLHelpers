e92c63b8e6d1590e60698ac59e94b45b10acc6e3
lambda0xff <chunchat.lam@icloud.com>
2022-09-07 20:53:08 +0200

    legend

--- helpers.py
+++ helpers.py
@@ -9,22 +9,24 @@ def plot_loss_curves(history):
   epochs = range(len(history.history["loss"]))
 
   # plot loss
-  plt.title("loss")
-  plt.xlabel("epochs")
-  plt.legend()
   plt.plot(epochs, loss, label="training loss")
   if "val_loss" in history.history.keys():
     val_loss = history.history["val_loss"]
     plt.plot(epochs, val_loss, label="validation loss")
 
+  plt.title("loss")
+  plt.xlabel("epochs")
+  plt.legend()
+
   # plot accuracy
   if "accuracy" in history.history.keys():
     accuracy = history.history["accuracy"]
     plt.figure()
-    plt.title("accuracy")
-    plt.xlabel("epochs")
-    plt.legend()
     plt.plot(epochs, accuracy, label="training accuracy")
     if "val_accuracy" in history.history.keys():
       val_accuracy = history.history["val_accuracy"]
       plt.plot(epochs, val_accuracy, label="validation accuracy")
+
+    plt.title("accuracy")
+    plt.xlabel("epochs")
+    plt.legend()
